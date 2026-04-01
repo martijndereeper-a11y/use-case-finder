677116eabf4c60b4f81a0d24f3e6e40ef32b9b0f
Martijn De reeper <martijndereeper@Mac.home>
2026-04-01 16:55:28 +0200

    Use JSON file for case data in serverless functions

--- api/chat.ts
+++ api/chat.ts
@@ -1,4 +1,4 @@
-import { useCases } from '../src/data';
+import useCases from '../use-cases-data.json';
 
 export default async function handler(req: any, res: any) {
   res.setHeader('Access-Control-Allow-Origin', '*');
